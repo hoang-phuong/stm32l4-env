ae503fc58aa742a594b24578cfba47d6bf39d99e
hoang-phuong <votruonghoangphuong@gmail.com>
2020-12-04 21:19:49 +0700

    update openocd version

--- make.bat
+++ make.bat
@@ -3,7 +3,7 @@ set OLD_PATH=%PATH%
 
 set TOOLCHAIN_PATH=C:/DevTools/gcc-arm-none-eabi-9-2020-q2-update-win32/bin
 set GNU_UTILS_PATH=C:/DevTools/msys64/usr/bin
-set  OPEN_OCD_PATH=C:/DevTools/OpenOCD-20200729-0.10.0/bin
+set  OPEN_OCD_PATH=C:/DevTools/OpenOCD-20200310-0.10.0/bin
 
 set MAKE_PATH=C:/DevTools/msys64/usr/bin
 
